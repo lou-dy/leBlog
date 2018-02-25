12868384a9c70489e155fa108a973e13766fea46
lou-dy <louise.dyquiangco@gmail.com>
2018-02-25 04:31:26 -0700

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :entries
 
-  root 'entries/index'
-  
-  get 'blog/index'
+  root 'entries#index'
+
+  get 'blog/index' => 'blog#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
