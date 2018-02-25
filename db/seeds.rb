0bc92c4cbb71fa1940c4f00bda1a8c2d0ef5fdf7
lou-dy <louise.dyquiangco@gmail.com>
2018-02-25 03:37:48 -0700

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create({
+ title: "My First Entry",
+ content: "Tuxedo cats always looking dapper plays league of legends. Jump around on couch, meow constantly until given food, crash against wall but walk away like nothing happened. Human is washing you why halp oh the horror flee scratch hiss bite show belly lick the other cats. Mewl for food at 4am prance along on top of the garden fence, annoy the neighbor's dog and make it bark attack the dog then pretend like nothing happened lick the curtain just to be annoying meow loudly just to annoy owners but Gate keepers of hell but my left donut is missing, as is my right. Kitty scratches couch bad kitty chase imaginary bugs, scratch play riveting piece on synthesizer keyboard love to play with owner's hair tie who's the baby.",
+ date: "2018-02-25"
+})
